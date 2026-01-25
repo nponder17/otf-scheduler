1cc4e73c564c10e779c54cff4007f04707859515
nponder17 <hq3276@wayne.edu>
2026-01-25 18:30:04 -0500

    Remove unused Navigate import

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ManagerSchedule from "./pages/ManagerSchedule";
 import EmployeeForm from "./pages/EmployeeForm";
 import AdminIndex from "./pages/AdminIndex";
